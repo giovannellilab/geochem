c71ad39f6de6e1d0105cae3a758136458ef94942
guillecg <guillemcg92@gmail.com>
2024-03-18 14:46:59 +0100

    Add Langelier-Ludwig plot as a separate function

---
+++ R/langelier-ludwig-plot.R
@@ -0,0 +1,18 @@
+plot_ll = function(df) {
+
+    # Add anions and cations transformations as columns
+    df = df %>%
+        mutate(
+            axis_anions=sqrt(2/3) * log(HCO3.meq / (Cl.meq * SO4.meq)),
+            axis_cations=log((Na.meq * K.meq) / (Ca.meq * Mg.meq))
+        )
+
+    ggplot(data=df) + 
+        geom_point(aes(x=axis_anions, y=axis_cations, color=axis_cations)) +
+        labs(
+            x=expression(
+                sqrt(frac(2, 3)) * log(frac("HCO"[3]^"-", "Cl"^"-" * "SO"[4]^"2-"))
+            ),
+            y=expression(log(frac("Na"^"+" * "K"^"+", "Ca"^"2+" * "Mg"^"2+")))
+        )
+}
