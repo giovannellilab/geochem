c6b278e0517e7b4bab5f61fe1eaf159ba102f400
guillecg <guillemcg92@gmail.com>
2024-04-09 20:10:27 +0200

    Add major cations ternary plot in Fullerton et al. 2021

---
+++ R/plot-major-cations.R
@@ -0,0 +1,46 @@
+library(ggplot2)
+library(ggtern)
+
+plot_base_major_cations = function() {
+
+    # Add division lines to the ternary plot
+    lines = data.frame(
+        x=c(0.5, 0.0, 0.5),
+        y=c(0.5, 0.5, 0.0),
+        z=c(0.0, 0.5, 0.5),
+        xend=c(1, 1, 1) / 3,
+        yend=c(1, 1, 1) / 3,
+        zend=c(1, 1, 1) / 3
+    )
+
+    # Create plot
+    plot = ggtern() +
+
+        # Add division lines
+        geom_segment(
+            data=lines,
+            aes(
+                x=x,
+                y=y,
+                z=z,
+                xend=xend,
+                yend=yend,
+                zend=zend
+            ),
+            color="black",
+            alpha=0.5,
+            linewidth=0.25,
+            linetype="dashed"
+        ) +
+
+        # Create the guides and labels
+        guides(fill=guide_legend(override.aes=list(shape=21))) +
+        labs(
+            x="Ca",
+            y="Mg",
+            z="Na + K",
+            fill="samples"
+        )
+
+    return(plot)
+}
