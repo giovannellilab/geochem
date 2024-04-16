0516f541100d25df3e765fb902caab5a4381cfd6
guillecg <guillemcg92@gmail.com>
2024-04-16 17:34:53 +0200

    Add initial Gibbs diagram

---
+++ R/plot-gibbs.R
@@ -0,0 +1,90 @@
+library(ggplot2)
+
+plot_base_gibbs = function() {
+    plot = ggplot() +
+
+        # Add lines
+        geom_segment(
+            aes(
+                x=0.05,
+                y=1e+2,
+                xend=0.9,
+                yend=10
+            ),
+            colour="lightgrey",
+            alpha=0.3
+        ) +
+        geom_segment(
+            aes(
+                x=0.05,
+                y=1e+3,
+                xend=0.9,
+                yend=1e+5
+            ),
+            colour="lightgrey",
+            alpha=0.3
+        ) +
+        geom_segment(
+            aes(
+                x=0.6,
+                y=1e+3,
+                xend=0.9,
+                yend=1e+2
+            ),
+            colour="lightgrey",
+            alpha=0.3
+        ) +
+        geom_segment(
+            aes(
+                x=0.6,
+                y=1e+3,
+                xend=0.9,
+                yend=1e+4
+            ),
+            colour="lightgrey",
+            alpha=0.3
+        ) +
+
+        # Add annotations
+        annotate(
+            geom="text",
+            x=0.2,
+            y=2e+2,
+            label="Rock",
+            color="black",
+            size=3,
+            alpha=0.6
+        ) +
+        annotate(
+            geom="text",
+            x=0.6,
+            y=1e+2,
+            label="Precipitation",
+            color="black",
+            size=3,
+            alpha=0.6
+        ) +
+        annotate(
+            geom="text",
+            x=0.4,
+            y=2e+4,
+            label="Evaporation",
+            color="black",
+            size=3,
+            alpha=0.6
+        ) +
+
+        # Format axes
+        scale_y_log10() +
+        annotation_logticks(sides="l") +
+
+        labs(
+            x="Na/(Na+Ca)",
+            y="TDS (ppm)",
+            fill="Temperature (°C)"
+        ) +
+
+        theme_glab()
+
+    return(plot)
+}
