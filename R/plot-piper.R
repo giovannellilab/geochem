0db1a2479a6b66205d2c9fae93bdbe4c0fd797fd
guillecg <guillemcg92@gmail.com>
2024-04-17 19:50:52 +0200

    Remove grid lines and colors from Piper plot

--- R/plot-piper.R
+++ R/plot-piper.R
@@ -43,6 +43,207 @@ plot_base_piper = function() {
     plot = ggplot() +
 
         # -------------------------------------------------------------------- #
+        # Add water regions (see https://inside.mines.edu/~epoeter/_GW/18WaterChem2/WaterChem2pdf.pdf)
+
+        # Na-HCO3 waters (deeper ground waters influenced by ion exchange)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(110, 85, 135),
+                y=c(17.3206,  60.6221, 60.6221)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Mixing zone (lower)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(110, 85, 135),
+                y=c(103.9236,  60.6221, 60.6221)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Na-Cl waters (marine and deep ancient ground waters)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(135, 110, 135, 160),
+                y=c(60.6221, 103.9236, 147.2251, 103.9236)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Ca-HCO3 waters (shallow, fresh ground waters)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(85, 60, 85, 110),
+                y=c(60.6221, 103.9236, 147.2251, 103.9236)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Ca-SO4 waters (gypsum ground waters and mine drainage)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(110, 85, 110, 135),
+                y=c(147.2251, 147.2251, 190.5266, 147.2251)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Mixing zone (upper)
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(110, 85, 135),
+                y=c(103.9236, 147.2251, 147.2251)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # -------------------------------------------------------------------- #
+        # Add cations regions
+
+        # Mg-Ca region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(0, 25, 50),
+                y=c(0, 43.3015, 0)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Mg-NA+K region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(25, 50, 75),
+                y=c(43.3015, 86.603, 43.3015)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Ca-NA+K region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(50, 75, 100),
+                y=c(0, 43.3015, 0)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # -------------------------------------------------------------------- #
+        # Add anions regions
+
+        # HCO3-Cl region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(120, 145, 170),
+                y=c(0, 43.3015, 0)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # HCO3-SO4 region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(145, 170, 195),
+                y=c(43.3015, 86.603, 43.3015)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # Cl-SO4 region
+        geom_polygon(
+            aes(
+                x=x,
+                y=y
+            ),
+            data=data.frame(
+                x=c(170, 195, 220),
+                y=c(0, 43.3015, 0)
+            ),
+            fill="white",
+            color="grey",
+            alpha=0.5,
+            size=0.25, # linewidth in newer ggplot2 versions
+        ) +
+
+        # -------------------------------------------------------------------- #
         # Add shapes
 
         # Left ternary plot (cations)
@@ -60,98 +261,6 @@ plot_base_piper = function() {
         geom_segment(aes(x=110, y=190.5266, xend=160, yend=103.9236)) +
         geom_segment(aes(x=110, y=17.3206,  xend=160, yend=103.9236)) +
         geom_segment(aes(x=110, y=17.3206,  xend=60,  yend=103.9236)) +
-
-        # -------------------------------------------------------------------- #
-        # Add grid lines
-
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(20, 40, 60, 80),
-                x2=c(10, 20, 30, 40),
-                y1=c(0, 0, 0, 0),
-                y2=c(17.3206, 34.6412, 51.9618, 69.2824)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(20, 40, 60, 80),
-                x2=c(60, 70, 80, 90),
-                y1=c(0, 0, 0, 0),
-                y2=c(69.2824, 51.9618, 34.6412, 17.3206)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(10, 20, 30, 40),
-                x2=c(90, 80, 70, 60),
-                y1=c(17.3206, 34.6412, 51.9618, 69.2824),
-                y2=c(17.3206, 34.6412, 51.9618, 69.2824)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(140, 160, 180, 200),
-                x2=c(130, 140, 150, 160),
-                y1=c(0, 0, 0, 0),
-                y2=c(17.3206, 34.6412, 51.9618, 69.2824)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(140, 160, 180, 200),
-                x2=c(180, 190, 200, 210),
-                y1=c(0, 0, 0, 0),
-                y2=c(69.2824, 51.9618, 34.6412, 17.3206)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(130, 140, 150, 160),
-                x2=c(210, 200, 190, 180),
-                y1=c(17.3206, 34.6412, 51.9618, 69.2824),
-                y2=c(17.3206, 34.6412, 51.9618, 69.2824)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(100, 90, 80, 70),
-                y1=c(34.6412, 51.9618, 69.2824, 86.603),
-                x2=c(150, 140, 130, 120),
-                y2=c(121.2442, 138.5648, 155.8854, 173.2060)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
-        geom_segment(
-            aes(x=x1, y=y1, yend=y2, xend=x2),
-            data=data.frame(
-                x1=c(70, 80, 90, 100),
-                y1=c(121.2442, 138.5648, 155.8854, 173.2060),
-                x2=c(120, 130, 140, 150),
-                y2=c(34.6412, 51.9618, 69.2824, 86.603)
-            ),
-            size=0.25,
-            colour="grey"
-        ) +
 
         # -------------------------------------------------------------------- #
         # Add axes ticks
@@ -313,183 +422,6 @@ plot_base_piper = function() {
             size=3.5,
             parse=TRUE
         ) + 
-
-        # -------------------------------------------------------------------- #
-        # Add water regions (see https://inside.mines.edu/~epoeter/_GW/18WaterChem2/WaterChem2pdf.pdf)
-
-        # Na-HCO3 waters (deeper ground waters influenced by ion exchange)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(110, 85, 135),
-                y=c(17.3206,  60.6221, 60.6221)
-            ),
-            fill="#2AB07F",
-            alpha=0.25
-        ) +
-
-        # Mixing zone (lower)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(110, 85, 135),
-                y=c(103.9236,  60.6221, 60.6221)
-            ),
-            fill="#5F6E5D",
-            alpha=0.1
-        ) +
-
-        # Na-Cl waters (marine and deep ancient ground waters)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(135, 110, 135, 160),
-                y=c(60.6221, 103.9236, 147.2251, 103.9236)
-            ),
-            fill="#482173",
-            alpha=0.25
-        ) +
-
-        # Ca-HCO3 waters (shallow, fresh ground waters)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(85, 60, 85, 110),
-                y=c(60.6221, 103.9236, 147.2251, 103.9236)
-            ),
-            fill="#FDE725",
-            alpha=0.25
-        ) +
-
-        # Ca-SO4 waters (gypsum ground waters and mine drainage)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(110, 85, 110, 135),
-                y=c(147.2251, 147.2251, 190.5266, 147.2251)
-            ),
-            fill="#1E9B8A",
-            alpha=0.25
-        ) +
-
-        # Mixing zone (upper)
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(110, 85, 135),
-                y=c(103.9236, 147.2251, 147.2251)
-            ),
-            fill="#5F6E5D",
-            alpha=0.1
-        ) +
-
-        # -------------------------------------------------------------------- #
-        # Add cations regions
-
-        # Mg-Ca region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(0, 25, 50),
-                y=c(0, 43.3015, 0)
-            ),
-            fill="#CB4149",
-            alpha=0.25
-        ) +
-
-        # Mg-NA+K region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(25, 50, 75),
-                y=c(43.3015, 86.603, 43.3015)
-            ),
-            fill="#390963",
-            alpha=0.25
-        ) +
-
-        # Ca-NA+K region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(50, 75, 100),
-                y=c(0, 43.3015, 0)
-            ),
-            fill="#85216B",
-            alpha=0.25
-        ) +
-
-        # -------------------------------------------------------------------- #
-        # Add anions regions
-
-        # HCO3-Cl region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(120, 145, 170),
-                y=c(0, 43.3015, 0)
-            ),
-            fill="#F5DB4C",
-            alpha=0.25
-        ) +
-
-        # HCO3-SO4 region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(145, 170, 195),
-                y=c(43.3015, 86.603, 43.3015)
-            ),
-            fill="#FCAE12",
-            alpha=0.25
-        ) +
-
-        # Cl-SO4 region
-        geom_polygon(
-            aes(
-                x=x,
-                y=y
-            ),
-            data=data.frame(
-                x=c(170, 195, 220),
-                y=c(0, 43.3015, 0)
-            ),
-            fill="#E65D2F",
-            alpha=0.25
-        ) +
 
         # -------------------------------------------------------------------- #
         # Remove axes
