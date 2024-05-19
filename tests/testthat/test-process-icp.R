d7367d0e6bf2bac93d5bb8d9ef502b072c43e1f5
guillecg <guillemcg92@gmail.com>
2024-05-19 20:53:35 +0200

    Add tests for select_icp_auto

--- tests/testthat/test-process-icp.R
+++ tests/testthat/test-process-icp.R
@@ -4,7 +4,7 @@ test_that("process_icp returns the same data.frame", {
   ICP_DIR = system.file("extdata", "PROJECT_NAME/icp-ms", package="geochem")
   
   icp_df_check = suppressMessages(
-     process_icp(
+    geochem::process_icp(
       filepath=file.path(ICP_DIR, ICP_FILENAME),
       blank_name=ICP_BLANK_NAME
     )
@@ -26,6 +26,43 @@ test_that("process_icp returns the same data.frame", {
   
   testthat::expect_equal(
     object=icp_df_check,
+    expected=icp_df_expected
+  )
+})
+
+test_that("select_icp_auto returns the same data.frame", {
+  ICP_FILENAME = "PROJECT_NAME.xlsx"
+  ICP_BLANK_NAME = "BLANK"
+  ICP_DIR = system.file("extdata", "PROJECT_NAME/icp-ms", package="geochem")
+  
+  icp_df_check = suppressMessages(
+    geochem::process_icp(
+      filepath=file.path(ICP_DIR, ICP_FILENAME),
+      blank_name=ICP_BLANK_NAME
+    )
+  )
+  icp_df_auto = geochem::select_icp_auto(
+    df=icp_df_check,
+    blank_name=ICP_BLANK_NAME
+  )
+  icp_df_expected = read.csv(test_path("testdata", "icp-data-auto.csv"))
+  
+  # Rename formatted columns while importing
+  icp_df_expected = icp_df_expected %>%
+    dplyr::rename(
+      `Conc. [ ppb ]`=`Conc....ppb..`,
+      `Conc. RSD`=`Conc..RSD`,
+      `CPS RSD`=`CPS.RSD`
+    ) %>%
+    dplyr::mutate(
+      # Add empty factor levels (removed in select_icp_auto)
+      sample=factor(sample, levels=levels(icp_df_check$sample)),
+      replicate=as.character(replicate),
+      isotope=as.character(isotope)
+    )
+  
+  testthat::expect_equal(
+    object=icp_df_auto,
     expected=icp_df_expected
   )
 })
