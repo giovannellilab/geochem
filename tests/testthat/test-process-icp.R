104589b331b93ae1d21d7ad6b1b69a21081fead4
guillecg <guillemcg92@gmail.com>
2024-05-19 20:45:23 +0200

    Add further processing to pass tests

--- tests/testthat/test-process-icp.R
+++ tests/testthat/test-process-icp.R
@@ -9,18 +9,23 @@ test_that("process_icp returns the same data.frame", {
       blank_name=ICP_BLANK_NAME
     )
   )
-  icp_df_check_expected = read.csv(test_path("testdata", "icp-data-check.csv"))
+  icp_df_expected = read.csv(test_path("testdata", "icp-data-check.csv"))
   
   # Rename formatted columns while importing
-  icp_df_check_expected = icp_df_check_expected %>%
+  icp_df_expected = icp_df_expected %>%
     dplyr::rename(
       `Conc. [ ppb ]`=`Conc....ppb..`,
       `Conc. RSD`=`Conc..RSD`,
       `CPS RSD`=`CPS.RSD`
+    ) %>%
+    dplyr::mutate(
+      sample=as.factor(sample),
+      replicate=as.character(replicate),
+      isotope=as.character(isotope)
     )
   
   testthat::expect_equal(
-    object=colnames(icp_df_check),
-    expected=colnames(icp_df_check_expected)
+    object=icp_df_check,
+    expected=icp_df_expected
   )
 })
