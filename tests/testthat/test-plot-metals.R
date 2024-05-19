f3ddfafc7576fcd5fec0d04242dc94629ec4b761
guillecg <guillemcg92@gmail.com>
2024-05-19 19:46:20 +0200

    Rename processed-data-icp.csv to icp-data-plot.csv

--- tests/testthat/test-plot-metals.R
+++ tests/testthat/test-plot-metals.R
@@ -1,5 +1,5 @@
 test_that("plot_metals returns the expected plot", {
-  df = read.csv(test_path("testdata", "processed-data-icp.csv"))
+  df = read.csv(test_path("testdata", "icp-data-plot.csv"))
   plot = geochem::plot_metals(df)
   vdiffr::expect_doppelganger(title="", fig=plot)
 })
